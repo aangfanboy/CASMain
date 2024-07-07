f7d49329b7da130c173a04bbf106c8ebb88e3ddc
burak <buraktoy161@gmail.com>
2024-07-07 18:17:31 +0300

    Initial check in

---
+++ ACConfig.m
@@ -0,0 +1,17 @@
+% Aircraft Configuration Data
+
+% Engine parameters
+max_throttle = 100.0; % Maximum throttle in N
+
+% Aerodynamic parameters
+drag_coefficient = 0.02; % Drag coefficient
+lift_coefficient = 1.5; % Lift coefficient
+
+% Mass and inertia
+aircraft_mass = 25; % Mass in kg
+moment_of_inertia = [5000, 6000, 7000]; % Moment of inertia in kg*m^2
+
+% Control limits
+max_aileron_deflection = 30; % Degrees
+max_elevator_deflection = 30; % Degrees
+max_rudder_deflection = 30; % Degrees
